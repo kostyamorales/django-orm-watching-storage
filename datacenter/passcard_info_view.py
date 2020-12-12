1353abcde183a2e311dc31dd5b5efa6b7c6f86d1
kostyamorales <kostyamorales@gmail.com>
2020-12-12 22:13:53 +0300

    Added data about the time os storage visits to the site

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,19 +1,19 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
+from datacenter.models import Passcard, Visit, get_duration, is_visit_long, format_duration
 from django.shortcuts import render
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
-
-    this_passcard_visits = [
-        {
-            "entered_at": "11-04-2018",
-            "duration": "25:03",
-            "is_strange": False
-        },
-    ]
+    passcard = Passcard.objects.filter(passcode=passcode)[0]
+    visits = Visit.objects.filter(passcard=passcard)
+    this_passcard_visits = []
+    for visit in visits:
+        duration = get_duration(visit)
+        flag = is_visit_long(visit)
+        this_passcard_visits.append({
+            "entered_at": visit.entered_at,
+            "duration": format_duration(duration),
+            "is_strange": flag
+        })
     context = {
         "passcard": passcard,
         "this_passcard_visits": this_passcard_visits
