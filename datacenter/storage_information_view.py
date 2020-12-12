b252add5452db79f23fee553db88eb759f9c54ea
kostyamorales <kostyamorales@gmail.com>
2020-12-12 22:04:42 +0300

    Posted on the site who and how long in the storage

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,18 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
+from datacenter.models import Passcard, Visit, get_duration, format_duration
 from django.shortcuts import render
 
 
 def storage_information_view(request):
-    # Программируем здесь
-
-    non_closed_visits = [
-        {
-            "who_entered": "Richard Shaw",
-            "entered_at": "11-04-2018 25:34",
-            "duration": "25:03",
-        }
-    ]
+    not_leaved_visits = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
+    for visit in not_leaved_visits:
+        duration = get_duration(visit)
+        non_closed_visits.append({
+            "who_entered": visit.passcard,
+            "entered_at": visit.entered_at,
+            "duration": format_duration(duration)
+        })
     context = {
-        "non_closed_visits": non_closed_visits,  # не закрытые посещения
+        "non_closed_visits": non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
