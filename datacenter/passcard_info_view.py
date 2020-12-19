d51fb00b31d9577ffb267eb759e99030dd59a308
kostyamorales <kostyamorales@gmail.com>
2020-12-19 13:50:47 +0300

    Fixed the code

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.filter(passcode=passcode)
+    passcard = Passcard.objects.get(passcode=passcode)
     visits = Visit.objects.filter(passcard=passcard)
     this_passcard_visits = []
     for visit in visits:
